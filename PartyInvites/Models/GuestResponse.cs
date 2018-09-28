16eb764be8d1ca462b962dd893981d9f22af742d
unknown <you@example.comdedorec2@my.lanecc.edu>
2018-09-28 11:23:08 -0700

    Tutorial complete

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+
+        #region properties
+        [Required(ErrorMessage = "Please Enter your Name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter your Email.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Enter Valid Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter your Phone.")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please Specify your Attendance.")]
+        public bool? WillAttend { get; set; }
+        #endregion
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+
+        #region fields
+        public static List<GuestResponse> responses = new List<GuestResponse>();
+        #endregion
+
+
+        #region methods
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+        #endregion
+    }
+}
